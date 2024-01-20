fac20da2b988e159898cb577314dfbd5783b6341
Parag1992 <paragbhatkar92@gmail.com>
2024-01-20 21:21:49 +0530

    buzzFuzz update

--- buzzfuzz.js
+++ buzzfuzz.js
@@ -9,6 +9,8 @@ let buzzFizz = () => {
             console.log("Fizz");
         }else if (i % 5 == 0){
             console.log("Buzz");
+        }else if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
         }else{
             console.log(i);
         }
